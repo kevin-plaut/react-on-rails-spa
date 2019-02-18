e8bc7a27d0bb8266f91bc984f5908d6f49cb89be
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2019-02-18 09:40:02 -0800

    yay! now loads bootstrap

--- app/javascript/packs/components/pages/LandingPage.js
+++ app/javascript/packs/components/pages/LandingPage.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import { Jumbotron } from 'react-bootstrap'
 import puppies from '../../assets/images/puppies.png'
 
 class LandingPage extends Component {
 	render () {
 		return (
-      <div className='text'>
+			<div className='text'>
         <h1>Hey there!</h1>
 				<br />
-				<h2>This is the landing page<br />...and these are my puppies!</h2>
+				<h3>This is the landing page<br />...and these are my puppies!</h3>
 				<img className='image' src={puppies} />
-      </div>
+			</div>
     )
   }
 }
