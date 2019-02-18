a941ac041d6574bd2269047c848614d750bf5150
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2019-02-18 01:08:06 -0800

    Add react-bootstrap and header

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Api } from './api/api'
+import Header from './components/shared/Header'
 import LandingPage from './components/pages/LandingPage'
 import About from './components/pages/About'
 
@@ -9,6 +10,7 @@ class App extends Component {
 		return (
       <div>
 				{Api.helloWorld()}
+				<Header />
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/about" component={About} />
