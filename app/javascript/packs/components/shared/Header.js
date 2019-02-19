30f163dd90de6e2d45af30ddb2faa15fe240e819
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2019-02-18 17:32:13 -0800

    header links work now too! :)

--- app/javascript/packs/components/shared/Header.js
+++ app/javascript/packs/components/shared/Header.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
 
 class Header extends Component {
 	render () {
 		return (
       <div>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+					<NavLink to="/">
+	          <Navbar.Brand href="/">React-on-Rails</Navbar.Brand>
+					</NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+							<NavLink to="/">
+              	<Nav.Link href="/">Home</Nav.Link>
+							</NavLink>
+							<NavLink to="/about">
+	              <Nav.Link href="/about">About</Nav.Link>
+							</NavLink>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
