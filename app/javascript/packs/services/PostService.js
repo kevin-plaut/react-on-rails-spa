377e298ab52fa9dec5b0ed144e4226ffec46b810
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2019-02-22 07:54:53 -0800

    ...further we go

--- app/javascript/packs/services/PostService.js
+++ app/javascript/packs/services/PostService.js
@@ -1,11 +1,14 @@
 import AuthService from './AuthService'
 
 export default class PostService extends AuthService {
-  createPost(post) {
+  createPost(post, user_id) {
     return this.fetch(`${this.domain}/posts`, {
       method: 'POST',
       body: JSON.stringify({
-        post: post
+        post: {
+          post,
+          user_id
+        }
     	})
     }).then((resp) => {
       let json = resp.json()
