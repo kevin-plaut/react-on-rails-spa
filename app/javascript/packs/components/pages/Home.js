7dec1c80f39ccc45ba4f5097ecffb293953b6e26
ChiefDakota <17812895+ChiefDakota@users.noreply.github.com>
2019-02-27 23:35:11 -0800

    fix spacing

--- app/javascript/packs/components/pages/Home.js
+++ app/javascript/packs/components/pages/Home.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react'
 import puppies1 from '../../assets/images/puppies-park-1.png'
 
 class Home extends Component {
-	render() {
-		return (
-			<div className="center">
+  render() {
+    return (
+      <div className="center">
         <h1>
-					Home
-				</h1>
-				<br />
-				<h3>
-					Hey there... check
-					<br />
-					out our puppies!
-				</h3>
-				<br />
-				<img className="image" src={puppies1} alt="Puppies at the park (1)"/>
-			</div>
+          Home
+        </h1>
+        <br />
+        <h3>
+          Hey there... check
+          <br />
+          out our puppies!
+        </h3>
+        <br />
+        <img className="image" src={puppies1} alt="Puppies at the park (1)"/>
+      </div>
     )
   }
 }
